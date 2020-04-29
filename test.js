dcfb2bdb92d6835f54d24bb4400d215b96111386
sonyoon7 <sonyoon7@gmail.com>
2020-04-29 14:16:39 +0900

    a

---
+++ test.js
@@ -0,0 +1,33 @@
+// import moment from 'moment';
+// import myLib from('./myLib');
+
+test("1 is 1", () => {
+    expect(1).toBe(1)
+})
+
+
+function getUser(id) {
+    return {
+        id,
+        email: `user${id}@test.com`,
+    }
+}
+
+// test("return a user object", () => {
+//     expect(getUser(1)).toBe({
+//         id: 1,
+//         email: `user1@test.com`,
+//     })
+// })
+
+test("return a user object", () => {
+    expect(getUser(1)).toEqual({
+        id: 1,
+        email: `user1@test.com`,
+    })
+})
+
+test("number 0 is falsy but string 0 is truthy", () => {
+    expect(0).toBeFalsy()
+    expect("0").toBeTruthy()
+})
